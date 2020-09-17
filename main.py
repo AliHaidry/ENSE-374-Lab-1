2bf910485c92c03b8c12716433cf5795ef672e1a
SyedMuhmammadAliHaidry <alihaidry11@gmail.com>
2020-09-17 08:01:52 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("Syed Muhammad Ali Haidry")
+print("Welcome to faculty Software Systems Engineering !")
+
+name = input("Your response please:  ")
+if name == 'enroll':
+  print("Done! You are enrolled in Software Engineering Management! ")
+elif name == 'drop' :
+  print("Oops! You are not enrolled in Software Engineering Management! ")
+
